ffbc3fd994083ce75a268bdc8aa5f5a2e836a413
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2022-11-22 20:10:06 +0100

    half way done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "faker"
+require "open-uri"
+
+Project.destroy_all
+# User.destroy_all
+
+# users = []
+
+# 5.times do
+#   user = User.new(
+#     # first_name: Faker::Name.first_name,
+#     # last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     password: "*************"
+#   )
+#   user.save!
+#   users << user
+# end
+
+
+20.times do
+  project = Project.new(
+    location: Faker::Address.country,
+  )
+  project.save!
+  puts "Project_location: #{project.location}"
+end
+puts "Cameras created"
