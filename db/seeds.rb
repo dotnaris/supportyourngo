4db02ae16da3dd0e0f7922f591847d6352986911
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2022-12-04 16:17:05 +0100

    seed file set-up

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,15 @@
 require "faker"
 require "open-uri"
 
-Project.destroy_all
-User.destroy_all
-Contribution.destroy_all
 Pledge.destroy_all
+Project.destroy_all
+Contribution.destroy_all
+User.destroy_all
+
 
 users = []
+
+# rand_paragraph = 'The French government has recently passed a law that will help improve the lives of cats in the country. The new law requires that all cat owners spay or neuter their pets, and it also bans the declawing of cats. This is great news for animal welfare advocates, as it will help reduce the number of homeless cats in France. It will also help improve the overall health of the cat population, as spaying and neutering can help prevent certain health problems. If you love cats and want to help make a difference in their lives, there are a few things you can do. You can spread the word about this new law and encourage others to spay or neuter their own cats. You can also donate to organizations that are working to help improve the lives of cats in France. Every little bit helps!'
 
 projects_name = ["Help our planet", "Plant a tree", "Help a child", "Make water accessible", "Water is a human right", "Save biodiversity", "Let's collect plastics from the ocean",
                  "Education for everyone", "We can do it", "Zero hunger", "Help pandas from extinction", "Clean up street garbages", "Hygiene for everyone", "Help us with creating paper cups",
@@ -24,10 +27,9 @@ projects_name = ["Help our planet", "Plant a tree", "Help a child", "Make water 
 amounts = ["50", "100,00", "200,00", "300,00", "600,00", "1200,00", "2400,00", "5800,00", "11,600,00", "23,200,00", "46,400,00", "92,800,00", "185,600,00",
            "371,200,00", "742,400,00", "148,400,00", "384,00", "768,00", "1,536,00", "3,072,00", "6,144,00", "12,288,00", "24,576,00", "49,152,00", "98,304,00", "196,608,00", "393,216,00", "786,432,00", "1,572,864,00"]
 
+################################################################################
 
-rand_numbers = [1..40]
-
-5.times do
+50.times do
   user = User.new(
     name: Faker::Name.name,
     email: Faker::Internet.email,
@@ -37,14 +39,16 @@ rand_numbers = [1..40]
   users << user
 end
 
-100.times do
+50.times do
   project = Project.new(
     name: projects_name.sample,
     location: Faker::Address.country,
-    user: users.sample
+    user: users.sample,
+    description: Faker::Quote.famous_last_words,
+    paragraph_title: 'The French government has recently passed a law that will help improve the lives of cats in the country. The new law requires that all cat owners spay or neuter their pets, and it also bans the declawing of cats. This is great news for animal welfare advocates, as it will help reduce the number of homeless cats in France. It will also help improve the overall health of the cat population, as spaying and neutering can help prevent certain health problems. If you love cats and want to help make a difference in their lives, there are a few things you can do. You can spread the word about this new law and encourage others to spay or neuter their own cats. You can also donate to organizations that are working to help improve the lives of cats in France. Every little bit helps!'
   )
   project.save!
-  puts "Project_location: #{project.location}, Project_name: #{project.name}, and Project_user: #{project.user}"
+  puts "Project_location: #{project.location}, Project_name: #{project.name}, and User_of_the_project: #{project.user.name}"
 end
 puts "Projects created"
 
@@ -55,15 +59,17 @@ puts "Projects created"
     amount: amounts.sample
   )
   contribution.save!
-  puts "user_who_contributed: #{contribution.user}, Project_name: #{contribution.project}, and amount: #{contribution.amount}"
+  puts "user_who_contributed: #{contribution.user.name}, Project_name: #{contribution.project.name}, and amount: #{contribution.amount}"
 end
 
 50.times do
   pledge = Pledge.new(
     title: projects_name.sample,
     reward: "Help #{Faker::Creature::Cat.breed}",
-    description: Faker::Creature::Cat.breed,
+    description: "20x cat food",
     project: Project.all.sample
   )
   pledge.save!
+  puts "Pledge_title: #{pledge.title}, Pledge_reward: #{pledge.reward}, Pledge_description: #{pledge.description},
+  and Pledge_title: #{pledge.project.name}"
 end
