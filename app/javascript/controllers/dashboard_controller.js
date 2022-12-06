ae51a5a646118da513a0af80949bf547eaf98fbb
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2022-12-06 05:29:45 +0100

    dashboard basic set up

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -2,11 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dashboard"
 export default class extends Controller {
-  static targets = ["status"]
+  static targets = ["status", "edit", "payment"]
   connect() {
   }
 
   status() {
-    console.log(this.statusTarget)
+    this.editTarget.classList.add('dashboard__edit__remove');
+    this.paymentTarget.classList.add('dashboard__payment__remove');
+    this.statusTarget.classList.remove('dashboard__remove');
+  }
+
+  edit() {
+
+    this.statusTarget.classList.add('dashboard__remove');
+    this.paymentTarget.classList.add('dashboard__payment__remove');
+    this.editTarget.classList.remove('dashboard__edit__remove');
+  }
+
+  payment() {
+    this.statusTarget.classList.add('dashboard__remove');
+    this.editTarget.classList.add('dashboard__edit__remove');
+    this.paymentTarget.classList.remove('dashboard__payment__remove');
   }
 }
