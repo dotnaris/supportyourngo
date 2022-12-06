780798c8300bb89dc467d8a2b582367b50a1ea1a
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2022-12-06 19:07:30 +0100

    seed file updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @Pledges = Pledge.all
+    @pledges = Pledge.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,22 @@ users = []
 
 # rand_paragraph = 'The French government has recently passed a law that will help improve the lives of cats in the country. The new law requires that all cat owners spay or neuter their pets, and it also bans the declawing of cats. This is great news for animal welfare advocates, as it will help reduce the number of homeless cats in France. It will also help improve the overall health of the cat population, as spaying and neutering can help prevent certain health problems. If you love cats and want to help make a difference in their lives, there are a few things you can do. You can spread the word about this new law and encourage others to spay or neuter their own cats. You can also donate to organizations that are working to help improve the lives of cats in France. Every little bit helps!'
 
+sample_titles = ["Support your NGO with a donation",
+                "Donate to an NGO today",
+                "NGOs that need financial help",
+                "How to donate money to an NGO",
+                "How to support an NGO in your community",
+                "How to support an organization through volunteering",
+                "How to support an organisation by donating time",
+                "How to support an NPO through donations",
+                "How to support an Non Profit Organization",
+                "How to support an non profit organization"]
+
+sample_texts = ["Support Your NGO is an innovative way for non-profit organizations to raise money for their projects. Fundraising campaigns are usually costly and time-consuming; Support Your NGO is a more cost-effective alternative that does not require organizers to travel to different communities. This makes it ideal for organizations that lack the resources or manpower to undertake conventional fundraising methods. It also provides a way for people who would normally feel unable to actively participate in organizational initiatives to become involved in making change happen",
+                "Support Your NGO is an acronym for non-profit organization fundraising utilizing the Internet. Support Your NGO's campaign is a method whereby non-profit organizations seek funding from a wide network of donors. Essentially, it's a way to run an organized donation drive without the expense of traveling to different communities. Donations are generally made online, and platforms allow individuals to donate money directly to the cause. In this essay, we will discuss the concept of Support Your NGO, outline the factors to consider when choosing a Support Your NGO platform, and provide advice on how to effectively promote your fundraising campaign.",
+                "Support Your NGO differs from conventional fundraising in several ways. Firstly, Support Your NGO sites are primarily focused on raising money for nonprofit organizations rather than business ventures. This means that the platform itself is chiefly concerned with helping non-profit organizations achieve their goals rather than making profit itself. Because of this, platforms typically have charity and cause-related themes such as food banks, animal shelters, and disaster relief campaigns. Since ngo fundraising is open to any non-profit organization, there are numerous possibilities for what can be funded and by whom.",
+                "Support Your NGO is an excellent method for non-profit organizations seeking funds for their projects without incurring costly conventional methods. While unconventional at first glance, innovative ngo sites offer great opportunities for both potential donors and nonprofit organizations alike. Considering the factors that determine which platform is best for you can make all the difference when successfully promoting a ngo fundraising campaign!"]
+
 projects_name = ["Help our planet", "Plant a tree", "Help a child", "Make water accessible", "Water is a human right", "Save biodiversity", "Let's collect plastics from the ocean",
                  "Education for everyone", "We can do it", "Zero hunger", "Help pandas from extinction", "Clean up street garbages", "Hygiene for everyone", "Help us with creating paper cups",
                  "Stop climate change", "Burger distribution challenge", "1.5 degrees", "Gender equality and equity", "No more fast fashion", "Spread some love", "Stop child abuse",
@@ -65,26 +81,51 @@ sleep 3
     name: projects_name.sample,
     location: Faker::Address.country,
     user: users.sample,
-    description: 'The French government has recently passed a law that will help improve the lives of cats in the country. The new law requires that all cat owners spay or neuter their pets, and it also bans the declawing of cats. This is great news for animal welfare advocates, as it will help reduce the number of homeless cats in France. It will also help improve the overall health of the cat population, as spaying and neutering can help prevent certain health problems. If you love cats and want to help make a difference in their lives, there are a few things you can do. You can spread the word about this new law and encourage others to spay or neuter their own cats. You can also donate to organizations that are working to help improve the lives of cats in France. Every little bit helps!',
-    paragraph_title: Faker::Quote.famous_last_words
+    description: sample_texts.sample,
+    paragraph_title: sample_titles.sample
   )
   project.save!
   counter2 += 1
-  puts "-------------------------"
+  puts ""
+  puts "========================="
+  puts ""
   puts "No. #{counter2}"
   puts "● Project_location: #{project.location}"
   puts "● Project_name: #{project.name}"
-  puts "● User_of_the_project: #{project.user.name}"
+  puts "● User_of_this_project: #{project.user.name}"
+  puts "● Project_paragraph_title: #{project.paragraph_title}"
+  puts "● Project_description: #{project.description}"
 
+  puts ""
+  puts "- - - - - -"
+  puts ""
+
+  pledge = Pledge.new(
+    title: project.name,
+    reward: project.paragraph_title,
+    description: "Choose your pledge. (e.g 10 x cat food..)",
+    project: project
+  )
+  pledge.save!
+  counter4 += 1
+  puts "No. #{counter4}"
+  puts "● Pledge_title: #{pledge.title}"
+  puts "● Pledge_reward: #{pledge.reward}"
+  puts "● Pledge_description: #{pledge.description}"
+  puts "● Project_ID_of_this_pledge: #{pledge.project.id}"
   sleep 0.01
 end
+puts ""
+puts "========================="
 
 puts ""
 puts "********** Woo Foo!!                  **********"
 puts "********** 50 Projects created ✔︎      **********"
 puts "**********      -  -                  **********"
+puts "********** And 50 Pledges created ✔︎   **********"
+puts "**********      -  -                  **********"
 puts ""
-sleep 3
+sleep 5
 
 50.times do
   contribution = Contribution.new(
@@ -106,31 +147,5 @@ end
 puts ""
 puts "********** Woo Foo!!                  **********"
 puts "********** 50 Contributions created ✔︎ **********"
-puts "**********      -  -                  **********"
-puts ""
-sleep 3
-
-50.times do
-  pledge = Pledge.new(
-    title: projects_name.sample,
-    reward: "Help #{Faker::Creature::Cat.breed}",
-    description: "20x cat food",
-    project: Project.all.sample
-  )
-  pledge.save!
-  counter4 += 1
-  puts "-------------------------"
-  puts "No. #{counter4}"
-  puts "● Pledge_title: #{pledge.title}"
-  puts "● Pledge_reward: #{pledge.reward}"
-  puts "● Pledge_description: #{pledge.description}"
-  puts "● Project_of_the_pledge: #{pledge.project.name}"
-
-  sleep 0.01
-end
-
-puts ""
-puts "********** Woo Foo!!                  **********"
-puts "********** 50 Pledges created ✔︎       **********"
-puts "**********      -  -                  **********"
+puts "************************************************"
 puts ""
