485ad7b8716f944828ea0e52dd1128b5d6709d67
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2022-12-08 18:03:48 +0100

    dahsboard backend halfway done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @pledges = Pledge.all
   end
+  def user_params
+    params.require(:user).permit(:name, :location )
+  end
 end
